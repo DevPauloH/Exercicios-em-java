748a9c1ecdf26e649ad8fd4bb3aafd2809553b54
DevPauloH <henriquedsg15@gmail.com>
2025-06-20 21:51:00 -0300

    Subindo código 1

---
+++ src/ParOuImpar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Olá! escolha um número e esse sistema irá responder se é par ou impar:");
+		int numeroDeEscolha = sc.nextInt();
+		
+		int resto = numeroDeEscolha % 2;
+		
+		if(resto == 0) {
+			System.out.println("Esse número é par!");
+		} else {
+			System.out.println("Esse número é impar!");
+		}
+
+	}
+
+}
